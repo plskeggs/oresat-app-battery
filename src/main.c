79f9a28d3bbca50f4d4d8f134c41a27a31c2ca7e
Pete Skeggs <plskeggs@noeticdesign.com>
2025-09-09 12:03:19 -0700

    Move main loop to thread

--- src/main.c
+++ src/main.c
@@ -16,9 +16,11 @@ LOG_MODULE_REGISTER(app_battery, LOG_LEVEL_DBG);
      DT_PROP_OR(DT_CHOSEN(zephyr_canbus), bus_speed, CONFIG_CAN_DEFAULT_BITRATE)         / \
      1000))
 
+#define BATT_THREAD_STACK_SIZE 512
+#define BATT_THREAD_PRIORITY 0
+
 int main(void)
 {
-	k_timepoint_t timepoint;
 	uint8_t node_id = oresat_get_node_id();
 
     LOG_INF("Oresat app battery starting up on board: %s, node: %u", CONFIG_BOARD_TARGET, (unsigned int)node_id);
@@ -30,12 +32,16 @@ int main(void)
     LOG_DBG("Initializing sensors");
 	board_sensors_init();
 
-    LOG_DBG("Starting main loop");
-	while (canopennode_is_running()) {
-		timepoint = sys_timepoint_calc(K_MSEC(1000));
-		board_sensors_fill_od();
-		k_sleep(sys_timepoint_timeout(timepoint));
-	}
+    K_THREAD_STACK_DEFINE(batt_stack, BATT_THREAD_STACK_SIZE);
+    struct k_thread batt_thread_data;
+
+    k_tid_t batt_thread = k_thread_create(&batt_thread_data, batt_stack, K_THREAD_STACK_SIZEOF(batt_stack),
+                                          batt_thread_handler, NULL, NULL, NULL,
+                                          BATT_THREAD_PRIORITY, 0, K_NO_WAIT);
+
+    // the battery thread is now running; wait until it exits, if ever, then clean up
+    k_thread_join(batt_thread, K_FOREVER);
+    k_thread_stack_free(batt_stack);
 
 	canopennode_stop(CAN_INTERFACE);
 	sys_reboot(SYS_REBOOT_COLD);
